b756d5694f74f5c902cb60344bcb161855f9daec
julissa almarazflores <jalmarazflores@my.yearupunited.org>
2026-04-13 20:41:58 -0500

    Added queries of actor and film tables

--- week-02/Ex1B_first_query.sql
+++ week-02/Ex1B_first_query.sql
@@ -3,3 +3,7 @@ Sakila is a database used to collect information about movies store/rental.
 It can be used to analyze how a rental store operates
 */
 
+USE sakila;
+SELECT * FROM actor; -- Retrieved 200 records
+
+SELECT * FROM film; -- Retrieved 1000 records

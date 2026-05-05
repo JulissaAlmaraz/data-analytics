d0407ebe908b37dcc9b744496dc7ad6877dc62b4
Julissa Almaraz <julissaalmaraz09@gmail.com>
2026-05-05 17:31:18 -0500

    Added varibles

--- week-05/Ex1A_hello.py
+++ week-05/Ex1A_hello.py
@@ -1,1 +1,21 @@
-print('Hello World')
+print('Hello world!')
+
+
+message = 'Hello world!'
+print(message)
+
+#Hello prints twice because we printed it in line 1 and also added it as a value on the variable message, and printed message.
+
+
+#Displaying dollars and cents:
+dollars = 3
+cents = 0.50
+print(dollars + cents)
+#It adds the value of dollars and cents together, printing 3.50
+
+cents = cents + .25
+print(dollars + cents)
+
+d_str = '3 dollars'
+c_str = '50 cents'
+print(d_str + ' ' + c_str)
